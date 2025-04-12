fbc6b8b290262ab97848646580a8aaf97f09a00e
twada0823 <twada698@gmail.com>
2025-04-12 15:21:40 +0900

    [update] メインスクリプトを修正し、アーティスト情報を表示する機能を追加

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from lyricidentifier-s46!")
-
-
-if __name__ == "__main__":
-    main()
+artist_id_dict = {"乃木坂46": 12550, "櫻坂46": 29512, "日向坂46": 22163}
+print(artist_id_dict)
+domain = "https://www.uta-net.com/artist/"
+artist_url_lst = []
+for artist_name, artist_id in artist_id_dict.items():
+    url = domain + str(artist_id) + "/"
+    artist_url_lst.append([artist_name, url])
+print(artist_url_lst)
